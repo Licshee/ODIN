38bf012a7d4b13fef0e9726e2251db427af61a70
NanaLich <NanaLich@users.noreply.github.com>
2015-07-09 21:27:45 +0800

    added universal portable libs

--- ODIN/OptimizedAsyncHandlerBase.cs
+++ ODIN/OptimizedAsyncHandlerBase.cs
@@ -3,25 +3,64 @@ using System.Threading.Tasks;
 
 namespace ODIN
 {
-#if !NET40
+#if NET45
     using TaskEx = Task;
 #endif
 
     public abstract class OptimizedAsyncHandlerBase<TInput, TArguments, TOutput>
         : OptimizedHandlerBase<TInput, TArguments, TOutput>
     {
-        protected readonly static Task<TOutput> DefaultResultTask = TaskEx.FromResult(default(TOutput));
+        protected readonly static Task<TOutput> DefaultResultTask;
 
+        static OptimizedAsyncHandlerBase()
+        {
+#if LegacyTask
+            var defaultTCS = new TaskCompletionSource<TOutput>();
+            defaultTCS.SetResult(default(TOutput));
+            DefaultResultTask = defaultTCS.Task;
+#else
+            DefaultResultTask = TaskEx.FromResult(default(TOutput));
+#endif
+        }
+
+#if LegacyTask
+        public Task<TOutput> ExecuteAsync(TInput input)
+        {
+            return ExtractArgumentsAsync(input)
+                .ContinueWith(argsTask =>
+                {
+                    var args = argsTask.Result;
+
+                    if (!IsCacheable(args))
+                        return ProcessAsync(args);
+
+                    return RetriveOptimizedAsync(args, input)
+                    .ContinueWith(resultTask =>
+                    {
+                        var result = resultTask.Result;
+                        if (result != null)
+                            return resultTask;
+
+                        return ProcessAsync(args);
+                    }).Unwrap();
+                    
+                }).Unwrap();
+        }
+#else
         public async Task<TOutput> ExecuteAsync(TInput input)
         {
             var args = await ExtractArgumentsAsync(input);
 
-            var result = await RetriveOptimizedAsync(args, input);
-            if (result != null)
-                return result;
+            if (IsCacheable(args))
+            {
+                var result = await RetriveOptimizedAsync(args, input);
+                if (result != null)
+                    return result;
+            }
 
             return await ProcessAsync(args);
         }
+#endif
 
         private bool _IsSyncExtractArgumentsOverridden = true;
         private bool _IsAsyncExtractArgumentsOverridden = true;
@@ -35,19 +74,30 @@ namespace ODIN
         {
             _IsSyncExtractArgumentsOverridden = false;
             EnsureExtractArgumentsOverridden();
-
+#if LegacyTask
+            return Task.Factory.StartNew(() => ExtractArgumentsAsync(input)).Unwrap().Result;
+#else
             return TaskEx.Run(() => ExtractArgumentsAsync(input)).Result;
+#endif
         }
         public virtual Task<TArguments> ExtractArgumentsAsync(TInput input)
         {
             _IsAsyncExtractArgumentsOverridden = false;
             EnsureExtractArgumentsOverridden();
 
+#if LegacyTask
+            return Task.Factory.StartNew(() => ExtractArguments(input));
+#else
             return TaskEx.Run(() => ExtractArguments(input));
+#endif
         }
 
         protected override TOutput RetriveOptimized(TArguments args, TInput input)
-            => TaskEx.Run(() =>RetriveOptimizedAsync(args, input)).Result;
+#if LegacyTask
+            => Task.Factory.StartNew(() => RetriveOptimizedAsync(args, input)).Unwrap().Result;
+#else
+            => TaskEx.Run(() => RetriveOptimizedAsync(args, input)).Result;
+#endif
         protected virtual Task<TOutput> RetriveOptimizedAsync(TArguments args, TInput input)
             => DefaultResultTask;
 
@@ -64,14 +114,22 @@ namespace ODIN
             _IsSyncProcessOverridden = false;
             EnsureProcessOverridden();
 
+#if LegacyTask
+            return Task.Factory.StartNew(() => ProcessAsync(args)).Unwrap().Result;
+#else
             return TaskEx.Run(() => ProcessAsync(args)).Result;
+#endif
         }
         protected virtual Task<TOutput> ProcessAsync(TArguments args)
         {
             _IsAsyncProcessOverridden = false;
             EnsureProcessOverridden();
 
+#if LegacyTask
+            return Task.Factory.StartNew(() => Process(args));
+#else
             return TaskEx.Run(() => Process(args));
+#endif
         }
     }
 }
--- ODIN/OptimizedHandlerBase.cs
+++ ODIN/OptimizedHandlerBase.cs
@@ -2,22 +2,28 @@
 {
     public abstract class OptimizedHandlerBase<TInput, TArguments, TOutput>
     {
-        public TOutput Execute(TInput input)
-        {
-            var args = ExtractArguments(input);
-
-            var result = RetriveOptimized(args, input);
-            if (result != null)
-                return result;
-
-            return Process(args);
-        }
-
         public abstract TArguments ExtractArguments(TInput input);
+
+        protected virtual bool IsCacheable(TArguments args)
+            => true;
 
         protected virtual TOutput RetriveOptimized(TArguments args, TInput input)
             => default(TOutput);
 
         protected abstract TOutput Process(TArguments args);
+
+        public TOutput Execute(TInput input)
+        {
+            var args = ExtractArguments(input);
+
+            if (IsCacheable(args))
+            {
+                var result = RetriveOptimized(args, input);
+                if (result != null)
+                    return result;
+            }
+
+            return Process(args);
+        }
     }
 }
